d2785e5a541a27ee7ead33433bb2f39dc220bfcd
AndrewSamoilov <andrew.smv94@gmail.com>
2025-04-26 10:20:54 +0300

    init

---
+++ src/main/kotlin/com/smv/budgetwiseapp/BudgetWiseAppApplication.kt
@@ -0,0 +1,11 @@
+package com.smv.budgetwiseapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BudgetWiseAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<BudgetWiseAppApplication>(*args)
+}
---
+++ src/test/kotlin/com/smv/budgetwiseapp/BudgetWiseAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.smv.budgetwiseapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BudgetWiseAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
